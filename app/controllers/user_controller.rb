93781d733e2ff8ab6b96dc2272f4fb60d608bcce
DavideDaniel <davidedaniel@gmail.com>
2015-03-12 17:26:12 -0400

    controller edits

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,13 +1,24 @@
 class UserController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    if params[:password] == params[:confirm_password]
+      @user = User.create(params_user)
+      redirect_to topics_path
+    else
+      redirect_to '/'
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :course_id, :linked_in, :twitter, :facebook, :github,  :password, :email, :avatar)
   end
 end
