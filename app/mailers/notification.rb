28d1b7c38b42dbc9dfa02aef9c425d0ac77395b8
Dan Snider <dansnider@gmail.com>
2015-03-12 12:00:02 -0400

    added send_event_notification.erb

--- app/mailers/notification.rb
+++ app/mailers/notification.rb
@@ -1,2 +1,11 @@
 class Notification < ApplicationMailer
+	# default :from => 'any_from_address@example.com'
+	
+	def send_event_notification(user)
+    @user = user
+    # If user's comedian books a new show in their area,
+			mail( :to => @user.email,	
+			:subject => "One of your favorite Comedians booked a show in your area" )
+   end
+
 end
