ef0c727602f1d8b1fcd3daa92af092709f7f2e16
Dan Snider <dansnider@gmail.com>
2015-03-13 15:54:56 -0400

    event_controller.rb new method

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -2,6 +2,8 @@ class EventController < ApplicationController
 	before_filter :authenticate_user!
 	# after_create Notification.send_event_notification(user)
   def new
+  	@event = Event.new
+  	Notification.send_event_notification(@event)
   end
 
   def create
