9c33f6362fbaa7681433d8931e07feffe7d234de
Dan Snider <dansnider@gmail.com>
2015-03-13 15:50:42 -0400

    notification.rb

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -1,5 +1,6 @@
 class EventController < ApplicationController
 	before_filter :authenticate_user!
+	# after_create Notification.send_event_notification(user)
   def new
   end
 
--- app/mailers/notification.rb
+++ app/mailers/notification.rb
@@ -1,11 +1,12 @@
 class Notification < ApplicationMailer
 	# default :from => 'any_from_address@example.com'
 	
-	def send_event_notification(user)
-    @user = user
-    # If user's comedian books a new show in their area,
-			mail( :to => @user.email,	
-			:subject => "One of your favorite Comedians booked a show in your area" )
+	def send_event_notification(event)
+    @event = event
+    @comedian = @event.comedian
+    @event.comedian.users.each do |user|
+			mail( :to => user.email,	
+			:subject => "#{@comedian} booked a show in your area!" )	
+    end
    end
-
 end
