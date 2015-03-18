250904de1a72f34cdd3725f3358eac09fe2b7cc9
DavideDaniel <davidedaniel@gmail.com>
2015-03-17 22:22:14 -0400

    removed address from new migration

--- db/migrate/20150317034927_add_location_data_to_events.rb
+++ db/migrate/20150317034927_add_location_data_to_events.rb
@@ -1,7 +1,6 @@
 class AddLocationDataToEvents < ActiveRecord::Migration
   def change
     add_column :events, :city, :string
-    add_column :events, :address, :string
     add_column :events, :state_code, :string
     add_column :events, :postal_code, :string
     add_column :events, :address, :string
