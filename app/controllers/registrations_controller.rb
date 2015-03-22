1a60e886138985a95c8c9688441a2471bb3aad74
DavideDaniel <davidedaniel@gmail.com>
2015-03-22 18:56:54 -0400

    adding changes for monday

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,14 +8,8 @@ class RegistrationsController < Devise::RegistrationsController
         resource.save
       end
 
-
         resource.update_without_password(params)
 
-    end
-
-
-    def update_resource(resource, params)
-      resource.update_without_password(params)
     end
 
   end
