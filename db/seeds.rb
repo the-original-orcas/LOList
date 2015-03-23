a540fe170c2d029e843563d23e101794ebac2821
DavideDaniel <davidedaniel@gmail.com>
2015-03-23 08:40:57 -0400

    fixing seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-# Comedian.destroy_all
-# Event.destroy_all
-# User.destroy_all
-
-# comedians = ["Kevin Hart", "Louis C.K.", "Aziz Ansari", "Jim Gaffigan", "Sarah Silverman", "Chelsea Peretti", "Nick Kroll", "Daniel Tosh", "John Oliver", "Hannibal Burress", "Dave Chappelle", "Amy Schumer"] 
-
 Comedian.create({
 	name: 'Kevin Hart',
 	image: 'http://i.imgur.com/xqIX6J0.jpg',
@@ -59,128 +45,6 @@ c5 = Comedian.create({
 	image: 'http://i.imgur.com/MznojsG.jpg',
 	youtube_url: 'https://www.youtube.com/embed/W2zTQwWaPps'
 	})
-
-# Event.create({
-# 	date: "2015-25-04",
-# 	time: "9:00",
-# 	venue: "Grand Sierra Resort and Casino",
-# 	price: 159.00,
-# 	city: "Reno",
-# 	state_code: "NV",
-# 	postal_code: "89595"
-# 	})
-
-# Event.create({
-# 	date: "2015-19-03",
-# 	time: "7:00",
-# 	venue: "Santa Cruz Civic Auditorium",
-# 	price: 166.00,
-# 	city: "Santa Cruz",
-# 	state_code: "CA",
-# 	postal_code: "95060"
-# 	})
-
-# Event.create({
-# 	date: "2015-29-03",
-# 	time: "7:00",
-# 	venue: "Comerica Theatre",
-# 	price: 152.00,
-# 	city: "Phoenix",
-# 	state_code: "AZ",
-# 	postal_code: "85003"
-# 	})
-
-# Event.create({
-# 	date: "2015-29-03",
-# 	time: "7:30",
-# 	venue: "Bryce Jordan Center",
-# 	price: 61.00,
-# 	city: "University Park",
-# 	state_code: "PA",
-# 	postal_code: "16802"
-# 	})
-
-# Event.create({
-# 	date: "2015-17-07",
-# 	time: "9:30",
-# 	venue: "McCaw Hall",
-# 	price: 84.00,
-# 	city: "Seattle",
-# 	state_code: "WA",
-# 	postal_code: "98109"
-# 	})
-
-# Event.create({
-# 	date: "2015-21-07",
-# 	time: "7:00",
-# 	venue: "Redding Convention Center",
-# 	price: 88.00,
-# 	city: "Redding",
-# 	state_code: "CA",
-# 	postal_code: "96001"
-# 	})
-
-# Event.create({
-# 	date: "2015-22-07",
-# 	time: "7:00",
-# 	venue: "Wells Fargo Center for the Art",
-# 	price: 119.00,
-# 	city: "Santa Rosa",
-# 	state_code: "CA",
-# 	postal_code: "95403"
-# 	})
-
-# Event.create({
-# 	date: "2015-23-07",
-# 	time: "7:00",
-# 	venue: "Mountain Winery",
-# 	price: 111.00,
-# 	city: "Saratoga",
-# 	state_code: "CA",
-# 	postal_code: "95070"
-# 	})
-
-# Event.create({
-# 	date: "2015-18-04",
-# 	time: "8:00",
-# 	venue: "Ryman Auditorium",
-# 	price: 52.00,
-# 	city: "Nashville",
-# 	state_code: "TN",
-# 	postal_code: "37219"
-# 	})
-
-# Event.create({
-# 	date: "2015-24-04",
-# 	time: "10:00",
-# 	venue: "Terry Fator Theatre at Mirage Hotel and Casino",
-# 	price: 143.00,
-# 	city: "Las Vegas",
-# 	state_code: "NV",
-# 	postal_code: "89109"
-# 	})
-
-# Event.create({
-# 	date: "2015-09-06",
-# 	time: "7:00",
-# 	venue: "Collins Center for the Arts",
-# 	price: 93.00,
-# 	city: "Orono",
-# 	state_code: "ME",
-# 	postal_code: "04469"
-# 	})
-
-# Event.create({
-# 	date: "2015-13-06",
-# 	time: "7:00",
-# 	venue: "Merrill Auditorium",
-# 	price: 106.00,
-# 	city: "Portland",
-# 	state_code: "ME",
-# 	postal_code: "04101"
-# 	})
-
-
 
 c4 = Comedian.create({
 	name: 'John Oliver',
@@ -302,3 +166,124 @@ u1.save
 # 	postal_code: '10001',
 # 	comedian_id: c3.id
 # 	})
+
+# Event.create({
+# 	date: "2015-25-04",
+# 	time: "9:00",
+# 	venue: "Grand Sierra Resort and Casino",
+# 	price: 159.00,
+# 	city: "Reno",
+# 	state_code: "NV",
+# 	postal_code: "89595"
+# 	})
+
+# Event.create({
+# 	date: "2015-19-03",
+# 	time: "7:00",
+# 	venue: "Santa Cruz Civic Auditorium",
+# 	price: 166.00,
+# 	city: "Santa Cruz",
+# 	state_code: "CA",
+# 	postal_code: "95060"
+# 	})
+
+# Event.create({
+# 	date: "2015-29-03",
+# 	time: "7:00",
+# 	venue: "Comerica Theatre",
+# 	price: 152.00,
+# 	city: "Phoenix",
+# 	state_code: "AZ",
+# 	postal_code: "85003"
+# 	})
+
+# Event.create({
+# 	date: "2015-29-03",
+# 	time: "7:30",
+# 	venue: "Bryce Jordan Center",
+# 	price: 61.00,
+# 	city: "University Park",
+# 	state_code: "PA",
+# 	postal_code: "16802"
+# 	})
+
+# Event.create({
+# 	date: "2015-17-07",
+# 	time: "9:30",
+# 	venue: "McCaw Hall",
+# 	price: 84.00,
+# 	city: "Seattle",
+# 	state_code: "WA",
+# 	postal_code: "98109"
+# 	})
+
+# Event.create({
+# 	date: "2015-21-07",
+# 	time: "7:00",
+# 	venue: "Redding Convention Center",
+# 	price: 88.00,
+# 	city: "Redding",
+# 	state_code: "CA",
+# 	postal_code: "96001"
+# 	})
+
+# Event.create({
+# 	date: "2015-22-07",
+# 	time: "7:00",
+# 	venue: "Wells Fargo Center for the Art",
+# 	price: 119.00,
+# 	city: "Santa Rosa",
+# 	state_code: "CA",
+# 	postal_code: "95403"
+# 	})
+
+# Event.create({
+# 	date: "2015-23-07",
+# 	time: "7:00",
+# 	venue: "Mountain Winery",
+# 	price: 111.00,
+# 	city: "Saratoga",
+# 	state_code: "CA",
+# 	postal_code: "95070"
+# 	})
+
+# Event.create({
+# 	date: "2015-18-04",
+# 	time: "8:00",
+# 	venue: "Ryman Auditorium",
+# 	price: 52.00,
+# 	city: "Nashville",
+# 	state_code: "TN",
+# 	postal_code: "37219"
+# 	})
+
+# Event.create({
+# 	date: "2015-24-04",
+# 	time: "10:00",
+# 	venue: "Terry Fator Theatre at Mirage Hotel and Casino",
+# 	price: 143.00,
+# 	city: "Las Vegas",
+# 	state_code: "NV",
+# 	postal_code: "89109"
+# 	})
+
+# Event.create({
+# 	date: "2015-09-06",
+# 	time: "7:00",
+# 	venue: "Collins Center for the Arts",
+# 	price: 93.00,
+# 	city: "Orono",
+# 	state_code: "ME",
+# 	postal_code: "04469"
+# 	})
+
+# Event.create({
+# 	date: "2015-13-06",
+# 	time: "7:00",
+# 	venue: "Merrill Auditorium",
+# 	price: 106.00,
+# 	city: "Portland",
+# 	state_code: "ME",
+# 	postal_code: "04101"
+# 	})
+
