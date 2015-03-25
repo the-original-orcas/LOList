5e2288bd295a4871d6193b277cb671534f4b2818
Dan Snider <dansnider@gmail.com>
2015-03-25 16:08:01 -0400

    fixed innerHTML problem?

--- app/assets/javascripts/countdown.js
+++ app/assets/javascripts/countdown.js
@@ -2,8 +2,10 @@ $(".user.show").ready(function() {
     var showDate = $('#date').val();
     var clock = document.getElementById("countdown-holder"),
         targetDate = new Date(showDate);
-    clock.innerHTML = countdown(targetDate).toString();
+
+    if (clock != null) {
+    	clock.innerHTML = countdown(targetDate).toString();
     setInterval(function() {
         clock.innerHTML = countdown(targetDate).toString();
-    }, 1000);
+    }, 1000);}
 });
