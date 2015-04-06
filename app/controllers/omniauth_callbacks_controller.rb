32c9102bd4dff3a0ca931cb6c10b6c29ff414717
DavideDaniel <davidedaniel@gmail.com>
2015-04-06 15:47:07 -0700

    identity.rb fixes

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,5 +1,5 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  
+
   def facebook
     generic_callback( 'facebook' )
   end
@@ -9,7 +9,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     @user = @identity.user || current_user
     if @user.nil?
-      @user = User.create( email: @identity.email || "" )
+      @user = User.create( email: @identity.email || "", image: @identity.image || "", location: @identity.location || "" )
       @identity.update_attribute( :user_id, @user.id )
     end
 
